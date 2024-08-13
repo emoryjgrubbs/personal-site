c7e5a934333f52e8f21889748bfa286aa1848b10
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-08-12 20:08:08 -0500

    !initialized project

---
+++ components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const config = useRuntimeConfig();
+
+function homeClick() {
+  window.location.href = `${config.public.BASEURL}/`;
+}
+function projectsClick() {
+  window.location.href = `${config.public.BASEURL}/projects`;
+}
+</script>
+
+<template>
+  <nav class="bg-nav text-text-light flex space-x-10 py-2">
+    <div class="flex grow">
+        <button
+          class="px-2"
+          title="Home"
+          @click="homeClick"
+        >
+          Home
+        </button>
+        <button
+          class="px-2"
+          title="Projects"
+          @click="projectsClick"
+        >
+          Projects
+        </button>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <body class="flex-auto bg-bg text-center">
+        <div class="inline-flex p-5 text-3xl">
+            Emory Grubbs
+        </div>
+    </body>
+</template>
