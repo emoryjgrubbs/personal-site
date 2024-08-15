8cef453dd164120916ddfdc7bd63b6bb221bb0d9
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-08-14 21:31:03 -0500

    !Added a contact information component

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
     <Navbar />
-    <div class="overscroll-none">
+    <div class="overscroll-none bg-bg text-text-dark">
         <NuxtPage />
+        <ContactInfo />
     </div>
 </template>
---
+++ components/ContactInfo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+const email = 'emoryjgrubbs@gmail.com';
+
+function handleEmail() {
+    navigator.clipboard.writeText(email);
+    //make some tool tip
+}
+
+</script>
+
+<template>
+    <div class="mt-5 mb-10">
+        <h1 class="underline">
+            Contact Information
+        </h1>
+        <div class="flex flex-row space-x-3 justify-evenly">  
+            <button
+            class=" justify-self-center"
+            title="Email Address"
+            @click="handleEmail">
+                Email: {{ email }}
+            </button>   
+        </div>
+    </div>
+</template>
