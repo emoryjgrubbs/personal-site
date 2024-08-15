7cce96c1f14919d8685c625f866a0575a9fc8aa0
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-08-15 13:28:23 -0500

    !Added search tags '$' and dates '@' -all search methods can only add results that match their criteria -multiple tags can be included as well as a title -tags are preceded by '$' and can be surrounded by quotes either '"' or ''' for multi word tags -dates are preceded by '@' and can also be surrouned by quotes, although the only information that matters is the numbers forming the date 	in the pattern 'YYYY.MM.DD' or 'YYYY.MM.DD.YYYY.MM.DD' where the dots are any character

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -4,7 +4,7 @@ const atc = {
     name: 'ATC Patient Data',
     desc: 'atc test description',
     thoughts: 'atc test thoughts',
-    tags: ['Autism treatment center', 'EPICs', 'UTD'],
+    tags: ['autism treatment center', 'epics', 'utd'],
     startDate: '2024-06-03',
     endDate: '2024-08-07',
     github: 'https://github.com/UTDallasEPICS/ATC-Patient-Data',
@@ -22,10 +22,220 @@ const personalSite = {
 const projects = [personalSite, atc];
 
 //search
-const searchTerm = ref();
+const searchTerm = ref("");
 
+function checkInclusion(project) { //note: this could cause slow downs, but should be fine with the limited number of projects
+    if(!searchTerm.value) {
+        return true;
+    }
+    if (project.name.toLowerCase().includes(searchTerm.value.toLowerCase())) {
+        return true;
+    }
+    // for over all letters in search term, if $ check tags, if @ check date
+    // build name from un-included elements in search term for the title search
+    let name = "";
+    let special = "";
+    let checkSpecial = "";
+    let quoted = false;
+    for (let i = 0; i < searchTerm.value.length; i++) {
+        // start tag, check special if one exists
+        if (searchTerm.value.at(i) === '$') {
+            if (checkSpecial === "tag" && checkTag(project, special)) {
+                return true;
+            }
+            if (checkSpecial === "date" && cheeckDate(project, special)) {
+                return true;
+            }
+            special = "";
+            checkSpecial = "tag";
+        }
+        // start date, check special if one exists
+        else if (searchTerm.value.at(i) === '@') {
+            if (checkSpecial === "tag" && checkTag(project, special)) {
+                return true;
+            }
+            if (checkSpecial === "date" && cheeckDate(project, special)) {
+                return true;
+            }
+            special = "";
+            checkSpecial = "date";
+        }
+        // if a special exists, add to it or do the search
+        else if (checkSpecial) {
+            if (searchTerm.value.at(i) === "\'" || searchTerm.value.at(i) === "\"") {
+                if (special === "") {
+                    quoted = true;
+                }
+                else {
+                    if (checkSpecial === "tag" && checkTag(project, special)) {
+                        return true;
+                    }
+                    if (checkSpecial === "date" && cheeckDate(project, special)) {
+                        return true;
+                    }
+                    special = "";
+                    checkSpecial = "";
+                    quoted = false;
+                }
+            }
+            // if quoted, include spaces
+            else if (quoted) {
+                special += searchTerm.value.at(i);
+            }
+            // if space, end and search, otherwise add element
+            else {
+                if (searchTerm.value.at(i) === " ") {
+                    if (checkSpecial === "tag" && checkTag(project, special)) {
+                        return true;
+                    }
+                    if (checkSpecial === "date" && cheeckDate(project, special)) {
+                        return true;
+                    }
+                    special = "";
+                    checkSpecial = "";
+                }
+                else {
+                    special += searchTerm.value.at(i);
+                }
+            }
+        }
+        // add to name
+        else {
+            // allow for space after quoted tag/date
+            if (!(i > 0 && (searchTerm.value.at(i-1) === "\'" || searchTerm.value.at(i-1) === "\""))) {
+                name += searchTerm.value.at(i)
+            }
+        }
+    }
+    // if search ended in a special search it
+    if (checkSpecial === "tag" && checkTag(project, special)) {
+        return true;
+    }
+    if (checkSpecial === "date" && cheeckDate(project, special)) {
+        return true;
+    }
+    if (name && name !== " " && project.name.toLowerCase().includes(name.toLowerCase())) {
+        return true;
+    }
+    return false;
+}
+function checkTag(project, tag) {
+    return project.tags.includes(tag.toLowerCase());
+}
+function cheeckDate(project, dateString) {
+    if (dateString.length == 21) {
+        let startDate = dateString.substring(0,10);
+        let endDate = dateString.substring(11,21);
+        // check if project start or end are in range
+        if(validateDate(startDate) && validateDate(endDate)) {
+            // project start is after end search date
+            if (project.startDate.substring(0,4) > endDate.substring(0,4)) {
+                return false;
+            }
+            else if (project.startDate.substring(0,4) == endDate.substring(0,4)){
+                if (project.startDate.substring(5,7) > endDate.substring(5,7)) {
+                    return false;
+                }
+                else if (project.startDate.substring(5,7) == endDate.substring(5,7)) {
+                    if (project.startDate.substring(8,10) > endDate.substring(8,10)) {
+                        return false;
+                    }
+                }
+            }
+            // project end is before the start search date
+            if (project.endDate !== "now") {
+                if (project.endDate.substring(0,4) < startDate.substring(0,4)) {
+                    return false;
+                }
+                else if (project.endDate.substring(0,4) == startDate.substring(0,4)){
+                    if (project.endDate.substring(5,7) < startDate.substring(5,7)) {
+                        return false;
+                    }
+                    else if (project.endDate.substring(5,7) == startDate.substring(5,7)) {
+                        if (project.endDate.substring(8,10) < startDate.substring(8,10)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        // check if project end is in range
+        else if (validate(startDate) && (endDate.toLowerCase().includes("now") || endDate.toLowerCase().includes("current") || endDate.toLowerCase().includes("ongoing"))) {
+            if (project.endDate !== "now") {
+                if (project.endDate.substring(0,4) < startDate.substring(0,4)) {
+                    return false;
+                }
+                else if (project.endDate.substring(0,4) == startDate.substring(0,4)){
+                    if (project.endDate.substring(5,7) < startDate.substring(5,7)) {
+                        return false;
+                    }
+                    else if (project.endDate.substring(5,7) == startDate.substring(5,7)) {
+                        if (project.endDate.substring(8,10) < startDate.substring(8,10)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (dateString.length == 10) {
+        let date = dateString.substring(0,10);
+        // check if project start and end cover date
+        if(validateDate(date)) {
+            // start is after date
+            if (project.startDate.substring(0,4) > date.substring(0,4)) {
+                return false;
+            }
+            else if (project.startDate.substring(0,4) == date.substring(0,4)){
+                if (project.startDate.substring(5,7) > date.substring(5,7)) {
+                    return false;
+                }
+                else if (project.startDate.substring(5,7) == date.substring(5,7)) {
+                    if (project.startDate.substring(8,10) > date.substring(8,10)) {
+                        return false;
+                    }
+                }
+            }
+            // end is before date
+            if (project.endDate !== "now") {
+                if (project.endDate.substring(0,4) < date.substring(0,4)) {
+                    return false;
+                }
+                else if (project.endDate.substring(0,4) == date.substring(0,4)){
+                    if (project.endDate.substring(5,7) < date.substring(5,7)) {
+                        return false;
+                    }
+                    else if (project.endDate.substring(5,7) == date.substring(5,7)) {
+                        if (project.endDate.substring(8,10) < date.substring(8,10)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+function validateDate(date) {
+    const datePattern = /\d{4}.\d{2}.\d{2}/; //regular expression of format YYYY_MM_DD
+    return datePattern.test(date);
+}
+
+//on click
 function handleProjectClick(project) {
     //take them to the project page
+    //fake by replacing the content, capture backbutton
 }
 function handleGithubClick(github) {
     window.location.href = `${github}`;
@@ -43,7 +253,7 @@ function handleGithubClick(github) {
             <input placeholder="Project Search" title="Project Search" v-model="searchTerm" />
         </div>
         <!--list of projects-->
-        <ul v-for="project in projects" v-show="!searchTerm | project.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())" class="py-5">
+        <ul v-for="project in projects" v-show="checkInclusion(project)" class="py-5">
             <div v-on:click="handleProjectClick(project)" class="hover:cursor-pointer">
                 <h1>
                     {{ project.name }}
