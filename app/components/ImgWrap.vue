04b6914a667977ae8185da96461bc031d423c93b
Emory Grubbs <emoryjgrubbs@gmail.com>
2025-08-15 19:43:33 -0700

    added details option to gallery + improved ImgWrap for real use

--- app/components/ImgWrap.vue
+++ app/components/ImgWrap.vue
@@ -1,22 +1,21 @@
 <template>
 	<div
-        v-if="props.crop"
-		class="flex aspect-square items-center overflow-hidden object-none select-none"
+		v-if="props.crop"
+		class="flex aspect-square cursor-pointer items-center overflow-hidden select-none"
 		@click="maximize"
 	>
 		<NuxtImg
+			class="h-full w-full object-cover"
 			:src="props.src"
 			:alt="props.alt"
 			loading="lazy"
 			draggable="false"
 		/>
 	</div>
-	<div
-        v-else
-		class="flex items-center overflow-hidden object-none select-none"
-		@click="maximize"
-	>
+	<div v-else>
 		<NuxtImg
+			class="cursor-pointer select-none"
+			@click="maximize"
 			:src="props.src"
 			:alt="props.alt"
 			loading="lazy"
@@ -29,21 +28,20 @@
 		class="fixed top-0 left-0 flex h-screen w-screen items-center justify-center select-none"
 	>
 		<div class="h-full w-full bg-black/45" @click="minimize"></div>
-		<div class="center-0 absolute flex h-fit w-fit">
+		<div class="center-0 absolute flex h-5/6 max-w-5/6">
 			<div
 				class="bg-alice-blue justify-self-right absolute -right-16 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full"
 				@click="minimize"
 			>
 				<Icon name="famicons:close" size="28" />
 			</div>
-			<div class="bg-alice-blue rounded-lg p-8">
-				<NuxtImg
-					:src="props.src"
-					:alt="props.alt"
-					loading="lazy"
-					draggable="false"
-				/>
-			</div>
+			<NuxtImg
+				class="bg-alice-blue max-h-full max-w-full self-center rounded-lg object-scale-down p-8"
+				:src="props.src"
+				:alt="props.alt"
+				loading="lazy"
+				draggable="false"
+			/>
 		</div>
 	</div>
 </template>
@@ -52,7 +50,7 @@
 const props = defineProps({
 	src: String,
 	alt: String,
-    crop: Boolean,
+	crop: Boolean,
 });
 
 const expanded = ref(false);
--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -9,30 +9,52 @@
 				@searchUpdate="(filterUpdate) => updateDisplay(filterUpdate)"
 				class="mb-10"
 			/>
-            <button class="justify-end flex w-full flex-row items-center gap-2 text-xl cursor-pointer"
-                    @click="toggleDetails"
-            >
-                <Icon
-                    v-if="showDetails"
-                    name="famicons:radio-button-on"
-                    class=""
-                />
-                <Icon
-                    v-else
-                    name="famicons:radio-button-off"
-                    class=""
-                />
-                Details
-            </button>
+			<button
+				class="flex w-full cursor-pointer flex-row items-center justify-end gap-2 text-xl"
+				@click="toggleDetails"
+			>
+				<Icon
+					v-if="showDetails"
+					name="famicons:radio-button-on"
+					class=""
+				/>
+				<Icon v-else name="famicons:radio-button-off" class="" />
+				Details
+			</button>
 
-            <div class="grid-cols-5 grid gap-3" v-if="!showDetails">
+			<div class="grid grid-cols-5 gap-3" v-if="!showDetails">
 				<div v-for="image in filteredImages">
-					<ImgWrap :src="image.src" :alt="image.alt" :crop="!showDetails"/>
+					<ImgWrap
+						:src="image.src"
+						:alt="image.alt"
+						:crop="!showDetails"
+					/>
 				</div>
 			</div>
-            <div class="grid-cols-1 grid gap-12" v-else>
+			<div class="grid grid-cols-1 gap-12" v-else>
 				<div v-for="image in filteredImages">
-					<ImgWrap :src="image.src" :alt="image.alt" :crop="!showDetails"/>
+					<div class="flex w-full flex-row gap-12">
+						<div class="w-3/8">
+							<ImgWrap
+								:src="image.src"
+								:alt="image.alt"
+								:crop="!showDetails"
+							/>
+						</div>
+						<div class="flex w-full flex-col gap-3 text-xl">
+							<h1 class="text-2xl">Title: {{ image.title }}</h1>
+							<div>Date: {{ image.dates[0].start }}</div>
+							<div class="flex flex-row flex-wrap gap-3">
+								Tags:
+								<div
+									v-for="tag in image.tags"
+									class="bg-columbia-blue rounded-md px-3"
+								>
+									{{ tag }}
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -42,27 +64,52 @@
 <script lang="ts" setup>
 const showDetails = ref(false);
 function toggleDetails() {
-    showDetails.value = !showDetails.value;
+	showDetails.value = !showDetails.value;
 }
 
 const images = [
 	{
 		title: "Seattle Fairy 1",
-		tags: ["seattle", "washington", "night", "water", "sound", "fairy", "color"],
+		tags: [
+			"seattle",
+			"washington",
+			"night",
+			"water",
+			"sound",
+			"fairy",
+			"color",
+		],
 		dates: [{ start: "2025-01-15", end: "2025-01-15" }],
 		src: "/images/test-img/boat-color.webp",
 		alt: "",
 	},
 	{
 		title: "Seattle Fairy 2",
-		tags: ["seattle", "washington", "night", "water", "sound", "fairy", "desaturated", "color"],
+		tags: [
+			"seattle",
+			"washington",
+			"night",
+			"water",
+			"sound",
+			"fairy",
+			"desaturated",
+			"color",
+		],
 		dates: [{ start: "2025-01-15", end: "2025-01-15" }],
 		src: "/images/test-img/boat-desat.webp",
 		alt: "",
 	},
 	{
 		title: "Window Reflection Abjuration",
-		tags: ["dallas", "texas", "utd", "university", "chromatic abjuration", "reflection", "color"],
+		tags: [
+			"dallas",
+			"texas",
+			"utd",
+			"university",
+			"chromatic abjuration",
+			"reflection",
+			"color",
+		],
 		dates: [{ start: "2025-06-26", end: "2025-06-26" }],
 		src: "/images/test-img/window-reflec.webp",
 		alt: "",
@@ -76,21 +123,47 @@ const images = [
 	},
 	{
 		title: "Bird Silhouette",
-		tags: ["austin", "texas", "animal", "bird", "birds", "sky", "black and white", "black & white", "b&k"],
+		tags: [
+			"austin",
+			"texas",
+			"animal",
+			"bird",
+			"birds",
+			"sky",
+			"black and white",
+			"black & white",
+			"b&k",
+		],
 		dates: [{ start: "2025-07-07", end: "2025-07-07" }],
 		src: "/images/test-img/bird-sil.webp",
 		alt: "",
 	},
 	{
 		title: "Liminal Gas Station (Narrow)",
-		tags: ["lorena", "texas", "gas", "gas station", "liminal", "night", "color"],
+		tags: [
+			"lorena",
+			"texas",
+			"gas",
+			"gas station",
+			"liminal",
+			"night",
+			"color",
+		],
 		dates: [{ start: "2025-07-09", end: "2025-07-09" }],
 		src: "/images/test-img/gas-narrow.webp",
 		alt: "",
 	},
 	{
 		title: "Liminal Gas Station (Wide)",
-		tags: ["lorena", "texas", "gas", "gas station", "liminal", "night", "color"],
+		tags: [
+			"lorena",
+			"texas",
+			"gas",
+			"gas station",
+			"liminal",
+			"night",
+			"color",
+		],
 		dates: [{ start: "2025-07-09", end: "2025-07-09" }],
 		src: "/images/test-img/gas-wide.webp",
 		alt: "",
@@ -111,28 +184,70 @@ const images = [
 	},
 	{
 		title: "More Shops Below (Color)",
-		tags: ["seattle", "washington", "people", "pike place", "pike place market", "market", "indoors", "night", "color"],
+		tags: [
+			"seattle",
+			"washington",
+			"people",
+			"pike place",
+			"pike place market",
+			"market",
+			"indoors",
+			"night",
+			"color",
+		],
 		dates: [{ start: "2025-01-09", end: "2025-01-09" }],
 		src: "/images/test-img/more-down-color.webp",
 		alt: "",
 	},
 	{
 		title: "More Shops Below (Black & White)",
-		tags: ["seattle", "washington", "people", "pike place", "pike place market", "market", "indoors", "night", "black and white", "black & white", "b&k"],
+		tags: [
+			"seattle",
+			"washington",
+			"people",
+			"pike place",
+			"pike place market",
+			"market",
+			"indoors",
+			"night",
+			"black and white",
+			"black & white",
+			"b&k",
+		],
 		dates: [{ start: "2025-01-09", end: "2025-01-09" }],
 		src: "/images/test-img/more-down-b&k.webp",
 		alt: "",
 	},
 	{
 		title: "Vancuver Chinatown at Night",
-		tags: ["vancuver", "bc", "british columbia", "night", "color", "neon", "neon sign", "wet", "chinatown"],
+		tags: [
+			"vancuver",
+			"bc",
+			"british columbia",
+			"night",
+			"color",
+			"neon",
+			"neon sign",
+			"wet",
+			"chinatown",
+		],
 		dates: [{ start: "2025-01-12", end: "2025-01-12" }],
 		src: "/images/test-img/chinatown-empty.webp",
 		alt: "",
 	},
 	{
 		title: "Lights of Downtown Vancuver",
-		tags: ["vancuver", "bc", "british columbia", "night", "color", "neon", "neon sign", "wet", "chinatown"],
+		tags: [
+			"vancuver",
+			"bc",
+			"british columbia",
+			"night",
+			"color",
+			"neon",
+			"neon sign",
+			"wet",
+			"chinatown",
+		],
 		dates: [{ start: "2025-01-12", end: "2025-01-12" }],
 		src: "/images/test-img/chinatown-cars.webp",
 		alt: "",
