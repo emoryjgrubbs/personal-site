50e5e2a803026c541940779fe364a5b506e7eff2
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2025-09-17 17:12:57 -0700

    return link moves according to scroll info and sticks out a bit to the left to stand out

--- app/components/ReturnLink.vue
+++ app/components/ReturnLink.vue
@@ -1,21 +1,40 @@
 <template>
 	<div
-		class="fixed top-33 z-40 mx-16 flex flex-col gap-y-8 transition ease-in-out hover:scale-110 md:mx-14 lg:mx-20"
+		class="fixed z-40 mx-12 flex flex-col gap-y-8 transition-all ease-out hover:scale-110 md:mx-8 xl:mx-20"
+		:class="y > 100 ? 'top-10' : y > 50 ? 'top-16' : 'top-33'"
 	>
 		<NuxtLink
 			:to="props.link"
-			class="flex cursor-pointer justify-evenly gap-2 rounded-lg bg-black py-2 pr-6 pl-4 text-center text-3xl font-semibold text-white"
+			class="flex cursor-pointer justify-evenly gap-2 rounded-lg bg-black py-2 text-center text-3xl font-semibold text-white transition-all ease-out"
+			:class="shorten ? 'pr-3 pl-2' : 'pr-6 pl-4'"
 			:title="'Return' + props.location"
 		>
 			<Icon name="famicons:caret-back" class="self-center" />
-			<div>{{ props.location }}</div>
+			<div v-show="!shorten">{{ props.location }}</div>
 		</NuxtLink>
 	</div>
 </template>
 
 <script lang="ts" setup>
+import { useWindowScroll } from "@vueuse/core";
+
+const { x, y, isScrolling, arrivedState, directions } = useWindowScroll({
+	behavior: "smooth",
+});
+
 const props = defineProps({
 	link: String,
 	location: String,
+});
+
+const shorten = ref(false);
+
+watch((y) => {
+	if (directions.top) {
+		shorten.value = false;
+	}
+	if (directions.bottom) {
+		shorten.value = true;
+	}
 });
 </script>
