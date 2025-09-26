841aa927d8af1225c5ee54c193df0efa2c1a6796
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2025-09-26 00:53:26 -0700

    added a simple route guard to prevent 404s

---
+++ app/middleware/01.routeGuard.global.ts
@@ -0,0 +1,31 @@
+import { navigateTo } from "nuxt/app";
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+	const router = useRouter();
+	const pages = router.getRoutes();
+
+	console.log(
+		"Attempting to navigating\nfrom: " + from.path + ", to: " + to.path
+	);
+
+	if (to.path == "/") {
+		return;
+	}
+
+	if (pages.map((element) => element.name).includes(to.name)) {
+		console.log("Route Exists, completing navigation");
+		return;
+	} else if (pages.map((element) => element.path).includes(from.path)) {
+		console.log("Route Does NOT Exist, redirecting to previous");
+		return abortNavigation(
+			createError({
+				statusCode: 404,
+				message: "Page Not Found, with no valid return page",
+				statusMessage: "Page Not Found",
+			})
+		);
+	} else {
+		console.log("No Valid Route Date, redirecting to root");
+		return navigateTo({ path: "/" });
+	}
+});
